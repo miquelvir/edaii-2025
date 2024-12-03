eedc4e774f152c1dc1cdff99fed6ee3f6de845c2
miquelvir <41950283+miquelvir@users.noreply.github.com>
2024-12-03 13:44:36 +0100

    Update sample_lib_test.c

--- test/sample_lib_test.c
+++ test/sample_lib_test.c
@@ -3,40 +3,42 @@
 
 void test_fact4(){
     runningtest("test_fact4");
-
-    int expectedResult = 24;
-    int result = fact(4);
-    assertEqualsInt(result, expectedResult);
-    
-    successtest("test_fact4");
+    {
+        int expectedResult = 24;
+        int result = fact(4);
+        assertEqualsInt(result, expectedResult);
+    }
+    successtest();
 }
 
 void test_fact3(){
     runningtest("test_fact3");
-
-    int expectedResult = 6;
-    int result = fact(3);
-    assertEqualsInt(result, expectedResult);
-    
-    successtest("test_fact3");
+    {
+        int expectedResult = 6;
+        int result = fact(3);
+        assertEqualsInt(result, expectedResult);
+    }
+    successtest();
 }
 
 
 void test_fact1(){
     runningtest("test_fact1");
-
-    int expectedResult = 1;
-    int result = fact(2);
-    assertEqualsInt(result, expectedResult);
-    
-    successtest("test_fact1");
+    {
+        int expectedResult = 2;
+        int result = fact(2);
+        assertEqualsInt(result, expectedResult);
+    }
+    successtest();
 }
 
 
 void sample_lib_test(){
     running("sample_lib_test");
-    test_fact4();
-    test_fact3();
-    test_fact1();
-    success("sample_lib_test");
+    {
+        test_fact4();
+        test_fact3();
+        test_fact1();
+    }
+    success();
 }
