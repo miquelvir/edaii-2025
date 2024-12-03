a1e9dcca14b72c2ea187790e00db2a30195a524a
miquelvir <41950283+miquelvir@users.noreply.github.com>
2024-12-03 18:31:23 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,7 +1,38 @@
 #include <stdio.h>
 #include "sample_lib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <sys/stat.h>
 
 int main(){
     printf("*****************\nWelcome to EDA 2!\n*****************\n");
+    
+    // how to import and call a function
     printf("Factorial of 4 is %d\n", fact(4));
+
+    // how to find all files in a folder
+    char* path = "./datasets/animals";
+    printf("The files in %s are:\n", path);
+    struct dirent *entry;
+    struct stat statbuf;
+    DIR *dp = opendir(path);
+
+    if (dp == NULL) {
+        perror("opendir");
+        return;
+    }
+
+    while ((entry = readdir(dp)) != NULL) {
+        char relativePath[1024];
+        snprintf(relativePath, sizeof(relativePath), "%s/%s", path, entry->d_name);
+
+        if (stat(relativePath, &statbuf) == -1 || !S_ISREG(statbuf.st_mode)) {
+            continue;
+        }
+
+        printf("  %s in %s\n", entry->d_name, relativePath);
+    }
+
+    closedir(dp);
 }
